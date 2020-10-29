e407b41ba8f3df76667ded9658d4ad693d364d41
jinimon <realpcy04@gmail.com>
2020-10-29 17:37:46 +0900

    디비 연동

---
+++ movie/src/main/java/com/cbox/common/DAO.java
@@ -0,0 +1,25 @@
+package com.cbox.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAO {
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String user = "jamong";
+	private String password = "1234";
+	
+	// private는 상속이 안되니까 connection 객체를 상속해야한다.
+	public Connection conn;	// db 연결 객체
+	
+	public DAO() {
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, password);	// 연결
+			System.out.println("zzzz");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
